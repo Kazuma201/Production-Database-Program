6846256ba31acb0b70f8ceafcfe999b2fc71f6f8
PV <b.kazuma0201@gmail.com>
2020-09-21 20:22:37 -0400

    Alpha

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -3,89 +3,87 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Tab;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class Controller
-{
-  //---------------------------------------------
-//ProductLine
-@FXML
-public void addProduct()
-{
-  System.out.println("Something");
-}
+public class Controller {
 
   @FXML
-  public void tblExistingPro(){}
+  private Label lblProductName;
 
   @FXML
-  public void lblExistingPro(){}
+  private Label lblManufacturer;
 
   @FXML
-  public void lblProductName(){}
+  private Label lblItemType;
 
   @FXML
-  public void lblManufacturer(){}
+  private TextField txtProductName;
 
   @FXML
-  public void lblItemType(){}
+  private TextField txtManufacturer;
 
   @FXML
-  public void txtProductName(){}
+  private ComboBox<?> cmboItemType;
 
   @FXML
-  public void txtManufacturer(){}
+  private Button btnAddProduct;
 
   @FXML
-  public void txtItemType(){}
-
-
-  @FXML
-  public void btnAddProduct(){}
-
-
-  //---------------------------------------------
-  //Produce
-  @FXML
-  public void comBoxChooseQuan(){}
+  private TableView<?> tblExistingPro;
 
   @FXML
-  public void recordProduction()
-  {
-    System.out.println();
+  private Label lblExistingPro;
+
+  @FXML
+  private Label lblChooseProduct;
+
+  @FXML
+  private ListView<?> lstProductList;
+
+  @FXML
+  private Label lblChooseQuantity;
+
+  @FXML
+  private ComboBox<String> comBoxChooseQuan;
+
+  @FXML
+  private Button btnRecordProduct;
+
+  @FXML
+  private TextArea txtArea;
+
+  @FXML
+  void addProduct(ActionEvent event) {
+    updateProduct();
   }
-  @FXML
-  public void lblChooseProduct(){}
 
   @FXML
-  public void lstProductList(){}
+  void recordProduction(ActionEvent event) {
 
-  @FXML
-  public void lblChooseQuantity(){}
-
-  @FXML
-  public void btnRecordProduct(){}
-  //ProductionLog
-  @FXML
-  public void txtArea(){}
-
+  }
   public void initialize()
   {
-    connectToDb();
+
+    comBoxChooseQuan.setEditable(true);
+    comBoxChooseQuan.getSelectionModel().selectFirst();
+
+    for(int count = 1; count <=10; count++) {
+      comBoxChooseQuan.getItems().add(String.valueOf(count));
+    }
   }
-  public static void connectToDb()
+
+  public void outputTable()
   {
     final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./resources/HRdb";
-
+    final String DB_URL = "jdbc:h2:./res/ProjectDB";
     //  Database credentials
     final String USER = "";
     final String PASS = "";
@@ -95,29 +93,72 @@ public void addProduct()
     try {
       // STEP 1: Register JDBC driver
       Class.forName(JDBC_DRIVER);
-
       //STEP 2: Open a connection
       conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
       //STEP 3: Execute a query
       stmt = conn.createStatement();
 
-      String sql = "SELECT * FROM JOBS";
+      String sql = "SELECT* FROM PRODUCT";
 
+      //Print out the Strings from the table
       ResultSet rs = stmt.executeQuery(sql);
-      while (rs.next()) {
-        System.out.println(rs.getString(1));
+      while(rs.next())
+      {
+        System.out.println(rs.getString(2));
+        System.out.println(rs.getString(3));
+        System.out.println(rs.getString(4));
       }
+
 
       // STEP 4: Clean-up environment
       stmt.close();
       conn.close();
     } catch (ClassNotFoundException e) {
       e.printStackTrace();
-
     } catch (SQLException e) {
       e.printStackTrace();
     }
   }
+
+
+  public void updateProduct()
+  {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/ProjectDB";
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    //Get the input text as strings
+    String productName = txtProductName.getText();
+    String manufacturer = txtManufacturer.getText();
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+
+
+      String sql = "INSERT INTO Product(type, manufacturer, name) "
+          + "VALUES ( 'AUDIO', '"+ productName + "', '" + manufacturer +"' )";
+
+      stmt.executeUpdate(sql);
+
+      outputTable();
+
+      // STEP 4: Clean-up environment
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
+}
